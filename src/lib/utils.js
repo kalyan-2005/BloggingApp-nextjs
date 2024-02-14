232a0d7bf2d7525fb66030c207ff203d245b3e09
kalyan-2005 <151056622+kalyan-2005@users.noreply.github.com>
2024-02-14 19:10:25 +0530

    Add files via upload

---
+++ src/lib/utils.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose")
+
+const connection = {};
+
+export const connectDb = async () => {
+    try {
+        if (connection.isConnected) {
+            console.log("Using existing connection")
+            return;
+        }
+        const db = await mongoose.connect(process.env.MONGO)
+        connection.isConnected = db.connections[0].readyState;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
